e34a2daad143cd22381560b37ffeeb5ce3979d8d
JamesVictor-o <victorjames40v@gmail.com>
2025-02-23 20:35:36 +0100

    feat:implemented the code-of-ethics section

--- src/components/guidelines/code-of-ethics.tsx
+++ src/components/guidelines/code-of-ethics.tsx
@@ -5,14 +5,14 @@ import ethicsData from "@/components/EthicsData";
 const CodeOfEthics: React.FC = () => {
   return (
     <main className="min-h-screen ">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-16 lg:py-20">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8  md:py-3 lg:py-3">
         <div className="max-w-7xl mx-auto space-y-10 md:space-y-12 lg:space-y-16  flex flex-col items-center">
           {/* Header Section */}
           <header className="space-y-6 md:space-y-5">
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white text-center      ">
               Code of Ethics
             </h1>
-            <p className="text-sm md:text-base lg:text-[15px] leading-relaxed md:leading-relaxed lg:leading-relaxed max-w-4xl mx-auto text-center px-4 md:px-6 lg:px-8 ">
+            <p className="text-sm md:text-base lg:text-[15px] leading-relaxed md:leading-relaxed lg:leading-relaxed max-w-3xl mx-auto text-center px-4 md:px-6 lg:px-8 ">
               At InheritX, we are committed to upholding the highest standards
               of integrity, security, and transparency. Our Code of Ethics
               ensures that our platform operates ethically, securely, and in
