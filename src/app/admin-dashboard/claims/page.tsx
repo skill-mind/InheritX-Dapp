846c28013787a305592b18e303a70a01cbd4b5b4
melvin <ianndiormel@gmail.com>
2025-03-24 10:09:04 +0100

    IMPLEMENT-THE-CLAIM-PAGE-ON-THE-MAINTAINER-DASHBOARD

--- src/app/admin-dashboard/claims/page.tsx
+++ src/app/admin-dashboard/claims/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from './components/code'
 
 export default function Claims() {
   return (
     <div>
-        Claims
+        <Dashboard />
     </div>
   )
 }
